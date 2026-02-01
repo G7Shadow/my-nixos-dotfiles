502023bab6dba61f737f1cc9a0eeffe4460ab0c4
G7Shadow <l.jeremy.822001@gmail.com>
2026-01-31 23:39:43 -0500

    ready start freah

---
+++ modules/home/programs/dotfiles.nix
@@ -0,0 +1,25 @@
+{
+  flake.homeModules.dotfiles = { config, ... }: 
+  let
+    dotfiles = "${config.home.homeDirectory}/my-nixos-dotfiles/modules/home/config";
+    create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+    
+    configs = {
+      hypr = "hypr";
+      rofi = "rofi";
+      waybar = "waybar";
+      kitty = "kitty";
+      matugen = "matugen";
+      nvim = "nvim";
+      tmux = "tmux";
+    };
+  in
+  {
+    xdg.configFile = builtins.mapAttrs
+      (name: subpath: {
+        source = create_symlink "${dotfiles}/${subpath}";
+        recursive = true;
+      })
+      configs;
+  };
+}
