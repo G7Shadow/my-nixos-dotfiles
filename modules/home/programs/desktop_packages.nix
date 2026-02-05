bb1a314902620b9880fb543f2c08983703064cf5
G7Shadow <l.jeremy.822001@gmail.com>
2026-02-04 21:24:22 -0500

    ready start freah

--- modules/home/programs/desktop_packages.nix
+++ modules/home/programs/desktop_packages.nix
@@ -1,89 +1,92 @@
- {
- flake.homeModules.desktop_packages = {pkgs, inputs, ...}:{
-
-  home.packages = with pkgs; [
+{
+  flake.homeModules.desktop_packages = {
+    pkgs,
+    inputs,
+    ...
+  }: {
+    home.packages = with pkgs; [
       # Dev tools
-    alejandra
-    cmake
-    gcc
-    opencode
-    gnumake
-    nodejs
-    unzip
-    git
-    jq
-    docker
+      alejandra
+      cmake
+      gcc
+      opencode
+      gnumake
+      nodejs
+      unzip
+      git
+      jq
+      docker
 
-    # Terminals & CLI utilities
-    alacritty
-    kitty
-    htop
-    btop
-    lsof
-    tree
-    wget
-    zoxide
-    ripgrep
-    fzf
-    bat
-    eza
-    fd
-    tmux
+      # Terminals & CLI utilities
+      alacritty
+      kitty
+      htop
+      btop
+      lsof
+      tree
+      wget
+      zoxide
+      ripgrep
+      fzf
+      bat
+      eza
+      fd
+      tmux
 
-    # Media & system utilities
-    libva
-    libva-utils
-    brightnessctl
-    lm_sensors
-    ffmpeg
-    pulsemixer
-    pwvucontrol
-    imagemagick
-    obs-studio
-    feh
-    nautilus
+      # Media & system utilities
+      libva
+      libva-utils
+      brightnessctl
+      lm_sensors
+      ffmpeg
+      pulsemixer
+      pwvucontrol
+      imagemagick
+      obs-studio
+      feh
+      nautilus
 
-    # Desktop tools (Wayland environment)
-    waybar
-    hyprpaper
-    wlogout
-    rofi
-    swww
-    swaynotificationcenter
-    wl-clipboard
-    cava
-    nwg-look
-    gamemode
-    mangohud
-    inputs.zen-browser.packages.${stdenv.hostPlatform.system}.default
-    vesktop
-    spotify
-    netflix
+      # Desktop tools (Wayland environment)
+      waybar
+      hyprpaper
+      wlogout
+      rofi
+      swww
+      swaynotificationcenter
+      wl-clipboard
+      cava
+      nwg-look
+      gamemode
+      mangohud
+      inputs.zen-browser.packages.${stdenv.hostPlatform.system}.default
+      vesktop
+      spotify
+      netflix
 
-    # Theming
-    pywal
-    matugen
-    bibata-cursors
-    nerd-fonts.jetbrains-mono
-    adwaita-icon-theme
+      # Theming
+      pywal
+      matugen
+      bibata-cursors
+      nerd-fonts.jetbrains-mono
+      adwaita-icon-theme
+      nwg-look
 
-    # Misc
-    playerctl
-    nitch
-    fastfetch
-    protonup-ng
+      # Misc
+      playerctl
+      nitch
+      fastfetch
+      protonup-ng
 
-    # Language servers
-    vscode-langservers-extracted
-    lua-language-server
-    typescript-language-server
-    nil
-    hyprls
-    pyright
+      # Language servers
+      vscode-langservers-extracted
+      lua-language-server
+      typescript-language-server
+      nil
+      hyprls
+      pyright
 
-    # Editors
-    vscode
-
-];
-};
+      # Editors
+      vscode
+    ];
+  };
 }
