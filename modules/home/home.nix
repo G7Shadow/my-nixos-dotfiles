a0432eada95b5b3a89d70d899b7c444d82c7d226
G7Shadow <l.jeremy.822001@gmail.com>
2026-02-23 19:47:40 -0500

    just some changes

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -23,20 +23,4 @@
       zsh
     ];
   };
-
-  # Standalone home-manager configuration (used with `home-manager switch`)
-  # Uses the same profile as the NixOS-integrated config
-  flake.homeConfigurations."jeremyl@Omega" = inputs.home-manager.lib.homeManagerConfiguration {
-    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-    extraSpecialArgs = {inherit inputs self;};
-    modules = [
-      self.homeModules.profile-desktop
-      {
-        home = {
-          username = "jeremyl";
-          homeDirectory = "/home/jeremyl";
-        };
-      }
-    ];
-  };
 }
