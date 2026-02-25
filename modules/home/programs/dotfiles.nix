222ef43fa3667210ef7b85d5927f0c8fa3666083
G7Shadow <l.jeremy.822001@gmail.com>
2026-02-24 21:00:24 -0500

    just some changes

--- modules/home/programs/dotfiles.nix
+++ modules/home/programs/dotfiles.nix
@@ -1,21 +1,23 @@
 {self, ...}: {
   flake.homeModules.dotfiles = {config, ...}: let
     dotfiles = "${config.home.homeDirectory}/my-nixos-dotfiles/modules/home/programs/config";
-    symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+    create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
 
-    link = subpath: {
-      source = symlink "${dotfiles}/${subpath}";
-      recursive = true;
+    configs = {
+      hypr = "hypr";
+      rofi = "rofi";
+      waybar = "waybar";
+      kitty = "kitty";
+      matugen = "matugen";
+      nvim = "nvim";
+      tmux = "tmux";
     };
   in {
-    xdg.configFile = {
-      hypr = link "hypr";
-      rofi = link "rofi";
-      waybar = link "waybar";
-      kitty = link "kitty";
-      matugen = link "matugen";
-      nvim = link "nvim";
-      tmux = link "tmux";
-    };
+    xdg.configFile =
+      builtins.mapAttrs
+      (name: subpath: {
+        source = create_symlink "${dotfiles}/${subpath}";
+      })
+      configs;
   };
 }
