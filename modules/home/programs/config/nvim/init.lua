347886bb62b58cf062b6b1bdcfbe0bf6fdf1fee0
G7Shadow <l.jeremy.822001@gmail.com>
2026-04-03 19:09:57 -0500

    just some changes

--- modules/home/programs/config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- init.lua
-require("core.keymaps")
-require("core.options")
-require("core.lazy")
-
-if not vim.g.vscode then
-  require("core.lsp")
-end
--- modules/home/programs/config/nvim/lua/plugins/lspconfig.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "neovim/nvim-lspconfig",
-  cond = not vim.g.vscode,
-  event = "BufReadPre",
-  config = function() end,
-}
