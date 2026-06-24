4cd6c69ebfd3a22cd880122fbfe0985bafc6d399
G7Shadow <l.jeremy.822001@gmail.com>
2026-06-24 11:47:48 -0500

    just some changes

--- modules/wrappedPrograms/dotfiles.nix
+++ modules/wrappedPrograms/dotfiles.nix
@@ -5,30 +5,29 @@
     let
       user = config.preferences.user.name;
       dotfiles = "/home/${user}/my-nixos-dotfiles/modules/nixos/features/config";
-          configs = {
-            hypr = "hypr";
-            quickshell = "quickshell";
-            nvim = "nvim";
-            kitty = "kitty";
-            rofi = "rofi";
-            swaync = "swaync";
-            matugen = "matugen";
-            waybar = "waybar";
-            tmux = "tmux";
-          };
-          links = builtins.concatStringsSep "\n" (
-            builtins.attrValues (
-              builtins.mapAttrs (name: sub: ''
-                ln -sfn "${dotfiles}/${sub}" "/home/${user}/.config/${name}"
-              '') configs
-            )
-          );
-        in
-        {
-          system.activationScripts.dotfileSymlinks = {
-            text = links;
-            deps = [ ];
-          };
-        };
+      configs = {
+        hypr = "hypr";
+        quickshell = "quickshell";
+        nvim = "nvim";
+        kitty = "kitty";
+        rofi = "rofi";
+        swaync = "swaync";
+        matugen = "matugen";
+        waybar = "waybar";
+        tmux = "tmux";
+      };
+      links = builtins.concatStringsSep "\n" (
+        builtins.attrValues (
+          builtins.mapAttrs (name: sub: ''
+            ln -sfn "${dotfiles}/${sub}" "/home/${user}/.config/${name}"
+          '') configs
+        )
+      );
+    in
+    {
+      system.activationScripts.dotfileSymlinks = {
+        text = links;
+        deps = [ ];
+      };
     };
 }
