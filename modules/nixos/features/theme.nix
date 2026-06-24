8de8577e70da41249f3940a3724e06c6f4fb1198
G7Shadow <l.jeremy.822001@gmail.com>
2026-06-24 13:45:33 -0500

    just some changes

--- modules/nixos/features/theme.nix
+++ modules/nixos/features/theme.nix
@@ -33,39 +33,17 @@
           "gtk-4.0/gtk.css".text = "@import 'colors.css'";
 
           "qt5ct/qt5ct.conf".text = ''
-            [Appearance]
-            color_scheme_path=/home/${user}/.config/qt5ct/colors/matugen.conf
+            color_scheme_path=~/.local/share/color-schemes/Matugen.colors
             custom_palette=true
-            style=Fusion
-
-            [Interface]
-            cursor_flash_time=1000
-            double_click_interval=400
-            keyboard_scheme=2
-            menus_have_icons=true
-            show_shortcuts_in_context_menus=true
-            stylesheets=@Invalid()
-            toolbutton_style=4
-            underline_shortcut=1
-            wheel_scroll_lines=3
+            icon_theme=breeze
+            style=<Breeze>
           '';
 
           "qt6ct/qt6ct.conf".text = ''
-            [Appearance]
-            color_scheme_path=/home/${user}/.config/qt6ct/colors/matugen.conf
+            color_scheme_path=~/.local/share/color-schemes/Matugen.colors
             custom_palette=true
-            style=Fusion
-
-            [Interface]
-            cursor_flash_time=1000
-            double_click_interval=400
-            keyboard_scheme=2
-            menus_have_icons=true
-            show_shortcuts_in_context_menus=true
-            stylesheets=@Invalid()
-            toolbutton_style=4
-            underline_shortcut=1
-            wheel_scroll_lines=3
+            icon_theme=breeze
+            style=<Breeze>
           '';
         };
       };
