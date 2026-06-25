80390b5867322e06ad59d8127f476c20682abeae
G7Shadow <l.jeremy.822001@gmail.com>
2026-06-25 13:32:54 -0500

    just some changes

--- modules/nixos/base/user.nix
+++ modules/nixos/base/user.nix
@@ -1,10 +1,18 @@
 {
-  flake.nixosModules.base = {lib, ...}: {
+  flake.nixosModules.base = {config, lib, pkgs, ...}: {
     options.preferences = {
       user.name = lib.mkOption {
         type = lib.types.str;
         default = "jeremyl";
       };
+    };
+
+    config = {
+      users.users."${config.preferences.user.name}" = {
+        isNormalUser = true;
+        group = "${config.preferences.user.name}";
+      };
+      users.groups."${config.preferences.user.name}" = {};
     };
   };
 }
