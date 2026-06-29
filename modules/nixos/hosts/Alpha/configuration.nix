aec6ed8adb6c3d30d413dcbc270854978625df01
G7Shadow <l.jeremy.822001@gmail.com>
2026-06-28 21:32:57 -0500

    just some changes

--- modules/nixos/hosts/Alpha/configuration.nix
+++ modules/nixos/hosts/Alpha/configuration.nix
@@ -18,7 +18,6 @@
       self.nixosModules.dotfiles
       self.nixosModules.git
       self.nixosModules.kitty
-      self.nixosModules.lf
       self.nixosModules.neovim
       self.nixosModules.quickshell
       self.nixosModules.theme
