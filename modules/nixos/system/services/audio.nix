dac8ab57b6267b2931ede318dc752570a1d82fb4
G7Shadow <l.jeremy.822001@gmail.com>
2026-06-29 23:06:42 -0500

    just some changes

--- modules/nixos/system/services/audio.nix
+++ modules/nixos/system/services/audio.nix
@@ -3,33 +3,11 @@
     security.rtkit.enable = true;
 
     services.pipewire = {
-      enable            = true;
-      alsa.enable       = true;
+      enable = true;
+      alsa.enable = true;
       alsa.support32Bit = true;
-      pulse.enable      = true;
+      pulse.enable = true;
 
-      extraConfig.pipewire."99-input-denoising"."context.modules" = [{
-        name = "libpipewire-module-filter-chain";
-        args = {
-          "node.description" = "DeepFilter Noise Cancelling Source";
-          "media.name" = "DeepFilter Noise Cancelling Source";
-          "filter.graph.nodes" = [{
-            type = "ladspa";
-            name = "DeepFilter Mono";
-            plugin = "${pkgs.deepfilternet}/lib/ladspa/libdeep_filter_ladspa.so";
-            label = "deep_filter_mono";
-          }];
-          "audio.rate" = 48000;
-          "capture.props" = {
-            "node.name" = "capture.deep_filter_mono";
-            "node.passive" = true;
-          };
-          "playback.props" = {
-            "node.name" = "deep_filter_mono_output";
-            "media.class" = "Audio/Source";
-          };
-        };
-      }];
     };
   };
 }
