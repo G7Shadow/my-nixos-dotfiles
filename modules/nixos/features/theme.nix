497f76b732c4b5bbf1f287f6108be819460e3c7d
G7Shadow <l.jeremy.822001@gmail.com>
2026-07-02 18:15:19 -0500

    just some changes

--- modules/nixos/features/theme.nix
+++ modules/nixos/features/theme.nix
@@ -13,7 +13,6 @@
         XCURSOR_SIZE = "24";
         HYPRCURSOR_THEME = "Bibata-Modern-Classic";
         HYPRCURSOR_SIZE = "24";
-        NIXOS_OZONE_WL = "1";
       };
 
       hjem.users."${user}" = {
