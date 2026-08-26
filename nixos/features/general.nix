b56645348a3f785ff7bf29fb2ae0d0fbbb309f34
G7Shadow <l.jeremy.822001@gmail.com>
2026-08-26 17:49:53 -0500

    some changes

--- nixos/features/general.nix
+++ nixos/features/general.nix
@@ -14,6 +14,7 @@
       ];
 
       programs.nix-index-database.comma.enable = true;
+      programs.fish.enable = true;
 
       users.users."${config.preferences.user.name}" = {
         isNormalUser = true;
