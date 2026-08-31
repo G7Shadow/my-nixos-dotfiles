e2fc65f2d7998e85702797d33f19add5ec263b3c
G7Shadow <l.jeremy.822001@gmail.com>
2026-08-31 11:36:39 -0500

    just some changes

--- wrappedPrograms/desktop-apps.nix
+++ wrappedPrograms/desktop-apps.nix
@@ -5,6 +5,18 @@
     { pkgs, config, ... }:
     let
       user = config.preferences.user.name;
+      zenProfiles = [
+        "hz3ab4gh.Default Profile"
+        "4hnn7g3c.Default Profile"
+      ];
+      chromeDir = "/home/${user}/my-nixos-dotfiles/nixos/features/config/zen";
+      mkProfileLinks = builtins.concatStringsSep "\n" (
+        map (profile: ''
+          mkdir -p "/home/${user}/.zen/${profile}/chrome"
+          cp "${chromeDir}/userChrome.js" "/home/${user}/.zen/${profile}/chrome/userChrome.js"
+          printf '%s\n' '// Enable userChrome.css and userChrome.js loading' 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' > "/home/${user}/.zen/${profile}/user.js"
+        '') zenProfiles
+      );
     in
     {
       services.gvfs.enable = true;
@@ -16,7 +28,6 @@
         spotify
         obsidian
         netflix
-        librewolf
         localsend
         prismlauncher
         zed-editor
@@ -27,10 +38,8 @@
         virt-manager
       ];
 
-      system.activationScripts.librewolfChrome = {
-        text = ''
-          mkdir -p "/home/${user}/.librewolf/default/chrome"
-        '';
+      system.activationScripts.zenHotReload = {
+        text = mkProfileLinks;
         deps = [ ];
       };
     }
