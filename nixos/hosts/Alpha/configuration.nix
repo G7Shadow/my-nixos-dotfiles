755e9200029c79e53fb042c06d41b1dd5e9c85cc
G7Shadow <l.jeremy.822001@gmail.com>
2026-09-10 14:40:46 -0500

    just some changes

--- nixos/hosts/Alpha/configuration.nix
+++ nixos/hosts/Alpha/configuration.nix
@@ -56,7 +56,7 @@
         enable = true;
         settings = {
           default_session = {
-            command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session";
+            command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session";
             user = "greeter";
           };
         };
