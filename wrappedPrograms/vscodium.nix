64db505984c7081da4b2e21c23187cc4ec59908b
G7Shadow <l.jeremy.822001@gmail.com>
2026-09-20 19:55:41 -0500

    some commits

--- wrappedPrograms/vscodium.nix
+++ wrappedPrograms/vscodium.nix
@@ -5,23 +5,25 @@
     { pkgs, config, ... }:
     let
       user = config.preferences.user.name;
+      marketplace = inputs'.nix-vscode-extensions.extensions.vscode-marketplace;
+      extensions = [
+        marketplace.jnoortheen.nix-ide
+        marketplace.mkhl.direnv
+        marketplace.kamadorueda.alejandra
+        marketplace.enkia.tokyo-night
+        marketplace.pkief.material-product-icons
+        marketplace.pkief.material-icon-theme
+        marketplace.asvetliakov.vscode-neovim
+        marketplace.theqtcompany.qt-qml
+      ];
     in
     {
-      hjem.users."${user}".packages =
-        let
-          marketplace = inputs'.nix-vscode-extensions.extensions.vscode-marketplace;
-        in
-        [
-          pkgs.vscodium
-          marketplace.jnoortheen.nix-ide
-          marketplace.mkhl.direnv
-          marketplace.kamadorueda.alejandra
-          marketplace.enkia.tokyo-night
-          marketplace.pkief.material-product-icons
-          marketplace.pkief.material-icon-theme
-          marketplace.asvetliakov.vscode-neovim
-          marketplace.theqtcompany.qt-qml
-        ];
+      hjem.users."${user}".packages = [
+        (pkgs.vscode-with-extensions.override {
+          vscode = pkgs.vscodium;
+          vscodeExtensions = extensions;
+        })
+      ];
     }
   );
 }
